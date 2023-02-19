2538b9d9675500507b84b8a066e4c1ebc9499cc7
Ramy Ouda <ramyouda@gmail.com>
2023-02-19 10:38:15 -0500

    route template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'game', component: GomeComponent },
+  // { path: 'scores', component: ScoresComponent },
+  // { path: '', redirectTo: 'game', pathMatch: 'full' },
+  // { path: '**', redirectTo: 'game', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
