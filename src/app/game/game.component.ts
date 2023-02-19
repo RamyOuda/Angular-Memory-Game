c21308f59ba3e83b0be7332db27957ac2c7f2119
Ramy Ouda <ramyouda@gmail.com>
2023-02-19 12:58:43 -0500

    update card colors in afterviewinit add updatescore method add shuffle method

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -20,31 +20,63 @@ interface Color {
 export class GameComponent implements OnInit, AfterViewInit {
   @ViewChild('boardRef') boardElement!: ElementRef;
 
-  colorArr: Color[] = [];
-
-  generateColors(): void {
-    const randomColor = () => Math.floor(Math.random() * 256);
-
-    for (let i = 0; i < 12; i++) {
-      this.colorArr.push({
-        R: randomColor(),
-        G: randomColor(),
-        B: randomColor(),
-      });
-    }
-  }
+  colors: Color[] = [];
+  currentScore = 0;
 
   ngOnInit(): void {
     this.generateColors();
   }
 
   ngAfterViewInit(): void {
-    const childrenArr = this.boardElement.nativeElement.children;
+    // Array of child elements of the board element
+    const children = this.boardElement.nativeElement.children;
 
-    for (let i = 0; i < childrenArr.length; i++) {
-      childrenArr[
-        i
-      ].style.backgroundColor = `rgb(${this.colorArr[i].R}, ${this.colorArr[i].G}, ${this.colorArr[i].B})`;
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      const color = this.colors[i];
+
+      child.style.backgroundColor = `rgb(${color.R}, ${color.G}, ${color.B})`;
     }
+  }
+
+  updateScore(): void {
+    this.currentScore++;
+  }
+
+  generateColors(): void {
+    // Function that returns a random number 0-255
+    const randomColor = () => Math.floor(Math.random() * 256);
+
+    // Add 6 objects of random RBG values into the colorArr array
+    for (let i = 0; i < 6; i++) {
+      this.colors.push({
+        R: randomColor(),
+        G: randomColor(),
+        B: randomColor(),
+      });
+    }
+
+    // Push a copy of the 6 objects into the array (for 2 of each color)
+    this.colors.push(...this.colors);
+
+    // Shuffle the array
+    this.shuffle(this.colors);
+  }
+
+  shuffle(array: Color[]): Color[] {
+    let currentIndex = array.length;
+    let randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
   }
 }
