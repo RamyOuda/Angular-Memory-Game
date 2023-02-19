0bd81cedeffb1d780b9d9ebae3e71d0ea2c38e0c
Ramy Ouda <ramyouda@gmail.com>
2023-02-19 10:50:49 -0500

    generate standalone color card component

---
+++ src/app/color-card/color-card.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ColorCardComponent } from './color-card.component';
+
+describe('ColorCardComponent', () => {
+  let component: ColorCardComponent;
+  let fixture: ComponentFixture<ColorCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ColorCardComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ColorCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/color-card/color-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-color-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './color-card.component.html',
+  styleUrls: ['./color-card.component.scss'],
+})
+export class ColorCardComponent {}
