1a8d62fd55cadd8be3a36d933f341f0f556b062a
Ramy Ouda <ramyouda@gmail.com>
2023-02-19 10:44:09 -0500

    add routing to game and scores components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { ScoresComponent } from './scores/scores.component';
 
 const routes: Routes = [
-  // { path: 'game', component: GomeComponent },
-  // { path: 'scores', component: ScoresComponent },
-  // { path: '', redirectTo: 'game', pathMatch: 'full' },
-  // { path: '**', redirectTo: 'game', pathMatch: 'full' },
+  { path: 'game', component: GameComponent },
+  { path: 'scores', component: ScoresComponent },
+  { path: '', redirectTo: 'game', pathMatch: 'full' },
+  { path: '**', redirectTo: 'game', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { ScoresComponent } from './scores/scores.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GameComponent, ScoresComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
